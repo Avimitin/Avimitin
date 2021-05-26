9b52b591c49d5b7020e6581dc2c2f6060e5ec1f5
avimitin <avimitin@gmail.com>
2021-05-26 22:41:09 +0800

    feat: added lazygit plugin

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 starship init fish | source
 
-export NNN_PLUG='j:autojump;p:preview-tui-ext;f:finder;o:fzopen;p:mocplay;d:diffs;t:nmount;v:imgview'
+export NNN_PLUG='g:lazygit;j:autojump;p:preview-tui-ext;f:finder;o:fzopen;p:mocplay;d:diffs;t:nmount;v:imgview'
 export NNN_FCOLORS="0B0B04060006060009060B06"
 set --export NNN_FIFO "/tmp/nnn.fifo"
 

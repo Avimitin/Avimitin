163f885b7d7ce7789af40d0a0a43971826059d75
avimitin <avimitin@gmail.com>
2021-05-26 20:08:04 +0800

    refactor: added NNN plugin environment setting

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,9 @@
 starship init fish | source
+
+export NNN_PLUG='j:autojump;p:preview-tui-ext;f:finder;o:fzopen;p:mocplay;d:diffs;t:nmount;v:imgview'
+export NNN_FCOLORS="0B0B04060006060009060B06"
+set --export NNN_FIFO "/tmp/nnn.fifo"
+
+alias ls n
+
+status --is-interactive; and source (jump shell fish | psub)

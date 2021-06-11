15927caf69606ecf4f87fa71b5a455a13f8fe775
avimitin <avimitin@gmail.com>
2021-06-11 23:41:11 +0800

    feat: prepend go path into $PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 starship init fish | source
 
+set PATH ~/go/bin $PATH
+
 export NNN_PLUG='g:lazygit;j:autojump;p:preview-tui-ext;f:finder;o:fzopen;p:mocplay;d:diffs;t:nmount;v:imgview'
 export NNN_FCOLORS="0B0B04060006060009060B06"
 set --export NNN_FIFO "/tmp/nnn.fifo"

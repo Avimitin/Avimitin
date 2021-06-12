ee9ae7050539c0afdf2266bd47af96f5d97de08c
avimitin <avimitin@gmail.com>
2021-06-12 10:26:40 +0800

    feat: added more documents for fish init

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,46 @@
-starship init fish | source
-
 set PATH ~/go/bin $PATH
 
-export NNN_PLUG='g:lazygit;j:autojump;p:preview-tui-ext;f:finder;o:fzopen;p:mocplay;d:diffs;t:nmount;v:imgview'
-export NNN_FCOLORS="0B0B04060006060009060B06"
-set --export NNN_FIFO "/tmp/nnn.fifo"
+# function to test executable exist
+function exec_exist
+	if test -z (command -v $argv)
+		return 1
+	end
 
-alias ls n
+	set -g has_$argv true
+	return 0
+end
 
-status --is-interactive; and source (jump shell fish | psub)
+# -_-test starship exist-_-
+if not exec_exist starship
+	echo "starship not found, install it from https://starship.rs/guide/#%F0%9F%9A%80-installation"
+	echo "or run command:"
+	echo "sh -c '\$(curl -fsSL https://starship.rs/install.sh)'"
+end
+
+if $has_starship
+	starship init fish | source
+end
+
+#----test nnn-----
+if not exec_exist nnn
+	echo "nnn is not installed yet, install it https://github.com/jarun/nnn/wiki/Usage"
+	set has_nnn false
+end
+
+if $has_nnn
+	export NNN_PLUG='g:lazygit;j:autojump;p:preview-tui-ext;f:finder;o:fzopen;p:mocplay;d:diffs;t:nmount;v:imgview'
+	export NNN_FCOLORS="0B0B04060006060009060B06"
+	set --export NNN_FIFO "/tmp/nnn.fifo"
+	alias ls n
+end
+
+#----test jump----
+if not exec_exist jump
+	echo "jump not found, install it from https://github.com/gsamokovarov/jump"
+	echo "or using go:"
+	echo "go get github.com/gsamokovarov/jump"
+end
+
+if $has_jump
+	status --is-interactive; and source (jump shell fish | psub)
+end

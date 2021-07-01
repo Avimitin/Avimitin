13999bac9f8e1f287a46af7d9ee2b3f47f307c72
avimitin <avimitin@gmail.com>
2021-07-01 13:03:26 +0800

    feat: replace jump with zoxide; bind ls to lsd
    Former-commit-id: 648fc04bd71ca88616f39377f47b132fcb8b83af

--- fish/config.fish
+++ fish/config.fish
@@ -40,26 +40,48 @@ if not exec_exist nnn
 end
 
 if $has_nnn
-	export NNN_PLUG='g:lazygit;j:autojump;p:preview-tui-ext;f:finder;o:fzopen;p:mocplay;d:diffs;t:nmount;v:imgview'
+	export NNN_PLUG_GIT='g:lazygit;d:diffs'
+	export NNN_PLUG_EXPLORE='j:autojump;f:finder;o:fzopen'
+	export NNN_PLUG_FILE='t:nmount'
+	export NNN_PLUG_VIEW='p:mocplay;v:imgview;p:preview-tui-ext'
+	export NNN_PLUG='$NNN_PLUG_GIT;$NNN_PLUG_EXPLORE;$NNN_PLUG_FILE;$NNN_PLUG_VIEW'
 	export NNN_FCOLORS="0B0B04060006060009060B06"
 	set --export NNN_FIFO "/tmp/nnn.fifo"
-	alias ls n
+end
+
+#---test lsd----
+if not exec_exist lsd
+	echo $COLOR_RED"lsd not found"$COLOR_NM
+	echo "Install nnn https://github.com/Peltoche/lsd#installation"
+end
+if $has_lsd
+	alias ls lsd
 end
 
 #----test jump----
-if not exec_exist jump
-	echo -e $COLOR_RED"jump not found"$COLOR_NM
-	echo "install it from https://github.com/gsamokovarov/jump"
-	echo "or using go:"
-	echo "go get github.com/gsamokovarov/jump"
-end
-
-if $has_jump
-	status --is-interactive; and source (jump shell fish | psub)
-end
+#if not exec_exist jump
+#	echo -e $COLOR_RED"jump not found"$COLOR_NM
+#	echo "install it from https://github.com/gsamokovarov/jump"
+#	echo "or using go:"
+#	echo "go get github.com/gsamokovarov/jump"
+#end
+#
+#if $has_jump
+#	status --is-interactive; and source (jump shell fish | psub)
+#end
 
 if test -x ~/scripts/fetch-host-ip.sh
 	set host_ip (~/scripts/fetch-host-ip.sh)
 	set -gx http_proxy "http://$host_ip:7890"
 	set -gx https_proxy "http://$host_ip:7890"
+end
+
+# ---test zoxide---
+if not exec_exist zoxide
+	echo -e $COLOR_RED"zoxide not found"$COLOR_NM
+	echo "install it from https://github.com/ajeetdsouza/zoxide"
+end
+
+if $has_zoxide
+	zoxide init fish | source
 end

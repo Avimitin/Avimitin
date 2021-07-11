fcf718273077edced4153fac65b57ba2d7dc97d7
avimitin <avimitin@gmail.com>
2021-07-11 22:44:35 +0800

    feat: reattach session if session exist for tmux
    Former-commit-id: 05bb53319d32f2c03ae85a138cda7dc0fb932d12

--- fish/functions/t.fish
+++ fish/functions/t.fish
@@ -1,8 +1,16 @@
+set DEFAULT_SESSION_NAME 'MUX-WORK'
+
 function t --description 'tmux'
 	if test -n "$argv"
 		tmux $argv
 		return
 	end
 
-	tmux new -s tmux
+	set DEFAULT_SESSION_RUNNING (tmux list-sessions | grep $DEFAULT_SESSION_NAME)
+	
+	if test -n "$DEFAULT_SESSION_RUNNING"
+		tmux a -t $DEFAULT_SESSION_NAME
+	else
+	  tmux new -s $DEFAULT_SESSION_NAME
+	end
 end

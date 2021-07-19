7b569b50cce73966d5451515d5f681d6acf9d239
avimitin <avimitin@gmail.com>
2021-07-19 20:03:16 +0800

    feat: added auto startx
    Former-commit-id: 8563aa72ea19c5c2e7048728979d5cb8fd5ef11c

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,8 @@
 source ~/.config/fish/startup.fish
+
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty > ~/.xorg.log 2>&1
+    end
+end

bbfa81126e2a17feb4c0db0b4db64eea9c0e4ee0
avimitin <avimitin@gmail.com>
2021-07-19 20:45:29 +0800

    refactor: delete output redirect

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,6 @@ source ~/.config/fish/startup.fish
 # Start X at login
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx -- -keeptty > ~/.xorg.log 2>&1
+        exec startx -- -keeptty
     end
 end

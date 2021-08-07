a6664929ee28df51ebf77d0daf451ca321c1017a
avimitin <avimitin@gmail.com>
2021-08-07 10:29:40 +0800

    refactor: comment out xinit
    Former-commit-id: 80da79fff11d86fccbbd3dc30802609c5b556001

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
 source ~/.config/fish/startup.fish
 
 # Start X at login
-if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx -- -keeptty
-    end
-end
+#if status is-login
+#    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+#        exec startx -- -keeptty
+#    end
+#end

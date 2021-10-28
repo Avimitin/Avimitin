c1dd3e7948debc0b9c8b16ea740bf69035b9edd0
Avimitin <avimitin@gmail.com>
2021-10-28 10:05:57 +0800

    fish/nnn: move nnn settings to the n function
    I found that this variable can be set in the n function. So no more
    environment pollution.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- fish/functions/n.fish
+++ fish/functions/n.fish
@@ -1,4 +1,9 @@
 function n --wraps nnn --description 'support nnn quit and change directory'
+    # test nnn exist
+    if not type nnn 2>&1 >/dev/null
+      echo "nnn not found"
+    end
+
     # Block nesting of nnn in subshells
     if test -n "$NNNLVL"
         if [ (expr $NNNLVL + 0) -ge 1 ]
@@ -23,7 +28,18 @@ function n --wraps nnn --description 'support nnn quit and change directory'
     # stty lwrap undef
     # stty lnext undef
 
-    set -x NNN_FIFO "/tmp/nnn.fifo"
+    # nnn settings
+    set -x ICONLOOKUP       1
+    set -x NNN_PLUG_GIT     'g:mine/lazygit;d:diffs'
+    set -x NNN_BMS          "e:$HOME/Codes;h:$HOME;d:$HOME/Documents;c:$HOME/.config;D:$HOME/Downloads/"
+    set -x NNN_PLUG_EXPLORE 'j:autojump;f:fzcd;o:fzopen'
+    set -x NNN_PLUG_FILE    't:nmount;k:nuke;n:mine/neovide-remote;u:mine/nvui-remote'
+    set -x NNN_PLUG_VIEW    'v:imgview;p:preview-tui;P:preview-tabbed;w:mine/wallpaper'
+    set -x NNN_PLUG         "$NNN_PLUG_GIT;$NNN_PLUG_EXPLORE;$NNN_PLUG_FILE;$NNN_PLUG_VIEW"
+    set -x NNN_FCOLORS      "0B0B04060006060009060B06"
+    set -x NNN_FIFO         "/tmp/nnn.fifo"
+
+    # exercute
     nnn -e $argv
 
     if test -e $NNN_TMPFILE

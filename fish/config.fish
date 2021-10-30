fe2a08eafdffb071e61ce9fca86e8a5fab65fc2f
Avimitin <avimitin@gmail.com>
2021-10-30 20:16:42 +0800

    fish/prompt: add git status
    I found it hard to identify a git directory. So I add this in prompt.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 1aa33513787639a79a06e79ef97ff7ecc0821380 [formerly 6e4b1337ba5c3784af604a89f54f30e59cbf4ea3]
    Former-commit-id: 24b650d07a40322e87fd9f30dd3a118764c3d668

--- fish/config.fish
+++ fish/config.fish
@@ -12,7 +12,13 @@ set -x fish_greeting ""
 function fish_prompt -d "Write out the prompt"
     set ok $status
     echo
-    printf '%s%s%s\n' (set_color cyan) (prompt_pwd) (set_color normal)
+    printf '%s%s%s' (set_color cyan) (prompt_pwd) (set_color normal)
+    set -g __fish_git_prompt_showdirtystate true
+    set -g __fish_git_prompt_color brblue
+    set -g __fish_git_prompt_char_dirtystate "ﱐ"
+    set -g __fish_git_prompt_color_dirtystate bryellow
+    printf '%s' (fish_git_prompt)
+    echo
     if test $ok -gt 0
       printf '%s %s' (set_color red) (set_color normal)
     else

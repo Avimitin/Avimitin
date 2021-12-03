0d39086f6c389ab139d24480bf92a5d0de5623da
Avimitin <avimitin@gmail.com>
2021-12-03 10:24:52 +0800

    fish: use tide instead of my prompt
    My prompt is pure and simple but it is hard to read git information. So
    I decide to use tide.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 13f51eaec3b24049ec9b693f673361b10e108c96 [formerly 754dd49a103efd57aea5b62fd6677697ccee0642]
    Former-commit-id: e368ab9806fa28e3d92b99406d12ad7518d91ae7

--- fish/config.fish
+++ fish/config.fish
@@ -23,17 +23,17 @@ function __set_up_git_prompt
   printf "%s%s%s" (set_color yellow) (fish_git_prompt) (set_color normal)
 end
 
-function fish_prompt -d "Write out the prompt"
-    set __ok $status
-    echo
-    __set_up_user_prompt
-    __set_up_pwd_prompt
-    __set_up_git_prompt
-    echo
-    if test $__ok -gt 0
-      printf '%sx %s' (set_color red) (set_color normal)
-    else
-      printf '%s> %s' (set_color blue) (set_color normal)
-    end
-    set -e __ok
-end
+#function fish_prompt -d "Write out the prompt"
+#    set __ok $status
+#    echo
+#    __set_up_user_prompt
+#    __set_up_pwd_prompt
+#    __set_up_git_prompt
+#    echo
+#    if test $__ok -gt 0
+#      printf '%sx %s' (set_color red) (set_color normal)
+#    else
+#      printf '%s> %s' (set_color blue) (set_color normal)
+#    end
+#    set -e __ok
+#end

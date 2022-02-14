30451d589e36c36962f1a052adaca7a3c1e61a51
Avimitin <avimitin@gmail.com>
2022-02-14 10:54:39 +0800

    fish: rewrite the fish configuration
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,93 @@
 set -x fish_greeting ""
+
+# ===================================================================
+# alias
+# ===================================================================
+if command -q starship
+  starship init fish | source
+end
+
+if command -q exa
+    alias ll "exa -l -@ -h --icons --group-directories-first"
+    alias lt "exa -l -T -L2 --icons"
+end
+
+if command -q zoxide
+    zoxide init fish | source
+end
+
+# G
+if command -q git
+    alias gp "git push"
+end
+
+if command -q lazygit
+    alias lg "lazygit"
+end
+
+if command -q rsync
+    alias rsync "rsync -azvhP"
+end
+
+if command -q ssh
+    # this fix tmux color
+    alias xssh "TERM=xterm-256color ssh"
+end
+
+if command -q tmux
+    alias ta 'tmux attach -t'
+    alias tad 'tmux attach -d -t'
+    alias ts 'tmux new-session -s'
+    alias tl 'tmux list-sessions'
+    alias tksv 'tmux kill-server'
+    alias tkss 'tmux kill-session -t'
+end
+
+if command -q python3
+    alias urldecode 'python3 -c "import sys, urllib.parse as up; print(up.unquote(sys.argv[1]))"'
+    alias urlencode 'python3 -c "import sys, urllib.parse as up; print(up.quote(sys.argv[1]))"'
+end
+
+if command -q nvim
+    alias vi "nvim"
+else if command -q vim
+    alias vi "vim"
+end
+
+# Y
+if command -q 'yt-dlp'
+    alias ytd "yt-dlp"
+end
+
+# ===================================================================
+# env
+# ===================================================================
+set -gx LANG en_US.UTF-8
+
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XDG_DATA_HOME $HOME/.local/share
+
+set -gx EDITOR 'nvim'
+set -gx VISUAL "$EDITOR "
+set -gx SYSTEMD_EDITOR $EDITOR
+set -gx PAGER 'less'
+
+set -gx BROWSER /usr/bin/xdg-open
+
+set -gx BAT_THEME "OneHalfDark"
+
+# ===================================================================
+# keybinding
+# ===================================================================
+
+# enable vi keybind
+fish_vi_key_bindings
+
+# set my keybind
+function fish_user_key_binding
+    bind --preset -M default 'H' beginning-of-line
+    bind --preset -M visual 'H' beginning-of-line
+    bind --preset -M default 'L' end-of-line
+    bind --preset -M visual 'L' end-of-line
+end

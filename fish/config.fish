0fcd2c122223cb45a9302071e11715fc8bc5ed6b
Avimitin <avimitin@gmail.com>
2022-07-31 13:46:07 +0800

    Split fish config to different part
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 95a9145e167980dc8dd87427f7db61aa9368b050 [formerly fd500653ac3622e825397e8f65e3f2e82f93a4a8]
    Former-commit-id: b01d98e0a303dda3ef5946a303769c35477aee65

--- fish/config.fish
+++ fish/config.fish
@@ -1,23 +1,4 @@
 # ===================================================================
-# env
-# ===================================================================
-set -gx LANG en_US.UTF-8
-
-set -gx XDG_CONFIG_HOME $HOME/.config
-set -gx XDG_CACHE_HOME $HOME/.cache
-set -gx XDG_DATA_HOME $HOME/.local/share
-
-set -gx EDITOR 'nvim'
-set -gx VISUAL "$EDITOR "
-set -gx SYSTEMD_EDITOR $EDITOR
-set -gx PAGER 'less'
-
-set -gx BROWSER /usr/bin/xdg-open
-
-set -gx BAT_THEME "OneHalfDark"
-
-
-# ===================================================================
 # alias
 # ===================================================================
 if command -q exa
@@ -124,6 +105,7 @@ end
 alias "systart" "sudo systemctl start"
 alias "systop" "sudo systemctl stop"
 alias "systat" "sudo systemctl status"
+alias "sysres" "sudo systemctl restart"
 
 # zellij
 
@@ -160,43 +142,3 @@ bind -M insert '\e;' escape
 # fish settings
 # ===================================================================
 set -x fish_greeting ""
-
-
-# ===================================================================
-# fish themes
-# ===================================================================
-# Kanagawa Fish shell theme
-# A template was taken and modified from Tokyonight:
-# https://github.com/folke/tokyonight.nvim/blob/main/extras/fish_tokyonight_night.fish
-set -l foreground DCD7BA
-set -l selection 2D4F67
-set -l comment 727169
-set -l red C34043
-set -l orange FF9E64
-set -l yellow C0A36E
-set -l green 76946A
-set -l purple 957FB8
-set -l cyan 7AA89F
-set -l pink D27E99
-
-# Syntax Highlighting Colors
-set -g fish_color_normal $foreground
-set -g fish_color_command $cyan
-set -g fish_color_keyword $pink
-set -g fish_color_quote $yellow
-set -g fish_color_redirection $foreground
-set -g fish_color_end $orange
-set -g fish_color_error $red
-set -g fish_color_param $purple
-set -g fish_color_comment $comment
-set -g fish_color_selection --background=$selection
-set -g fish_color_search_match --background=$selection
-set -g fish_color_operator $green
-set -g fish_color_escape $pink
-set -g fish_color_autosuggestion $comment
-
-# Completion Pager Colors
-set -g fish_pager_color_progress $comment
-set -g fish_pager_color_prefix $cyan
-set -g fish_pager_color_completion $foreground
-set -g fish_pager_color_description $comment

c9b53ae72503aa9ae05a4144d27ca2f1a2e04ccf
Avimitin <avimitin@gmail.com>
2022-09-07 18:06:23 +0800

    Update wezterm config
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: 9f35f246008069395a9f8cb58291809652bf2d7d [formerly 044b87370abfc3b2f33552a3428d2c29aba21a3e]
    Former-commit-id: ed957618ddffe1a5b52b483fe86aacb0fde0b57f

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,16 +3,17 @@ local api = require("wezterm")
 return {
 	font = api.font("BlexMono Nerd Font Mono"),
 	font_size = 11,
-	window_background_opacity = 0.8,
+	window_background_opacity = 0.75,
 	force_reverse_video_cursor = true,
 	window_padding = {
 		left = 0,
-		right = 2,
+		right = 15,
 		top = 0,
 		bottom = 0,
 	},
 	enable_scroll_bar = true,
 	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
 	keys = {
 		{
 			key = "n",
@@ -29,22 +30,5 @@ return {
 			}),
 		},
 	},
-	colors = {
-		foreground = "#dcd7ba",
-		background = "#1f1f28",
-
-		cursor_bg = "#c8c093",
-		cursor_fg = "#c8c093",
-		cursor_border = "#c8c093",
-
-		selection_fg = "#c8c093",
-		selection_bg = "#2d4f67",
-
-		scrollbar_thumb = "#16161d",
-		split = "#16161d",
-
-		ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-		brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-		indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
-	},
+	color_scheme = "tokyonight-storm"
 }

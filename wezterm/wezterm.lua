e7e2d5aae59d206389fa9128f1bc9a5de0db6701
Avimitin <avimitin@gmail.com>
2022-09-19 15:55:50 +0800

    Update wezterm config
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: f1470ae09a1cad0c1709307365ec8899857ceb20 [formerly b9ded1768fd3d6d704f7ae005e82b6b37a3c5cc5]
    Former-commit-id: cee2b9b1dadb799310ee16a9f068679f985c1df4

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,13 +1,34 @@
 local api = require("wezterm")
 
+local kanagawa = {
+	foreground = "#dcd7ba",
+	background = "#1f1f28",
+
+	cursor_bg = "#c8c093",
+	cursor_fg = "#c8c093",
+	cursor_border = "#c8c093",
+
+	selection_fg = "#c8c093",
+	selection_bg = "#2d4f67",
+
+	scrollbar_thumb = "#16161d",
+	split = "#16161d",
+
+	ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+	brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+	indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+}
+
 return {
 	font = api.font_with_fallback({
 		"mononoki Nerd Font Mono",
 		"JetbrainsMono Nerd Font Mono",
+		"LXGW WenKai Mono",
 		"Noto Sans CJK SC",
 	}),
 	font_size = 11,
-	window_background_opacity = 0.75,
+	window_background_opacity = 0.8,
+	text_background_opacity = 0.2,
 	force_reverse_video_cursor = true,
 	window_padding = {
 		left = 0,
@@ -34,5 +55,5 @@ return {
 			}),
 		},
 	},
-	color_scheme = "tokyonight-storm"
+	colors = kanagawa
 }

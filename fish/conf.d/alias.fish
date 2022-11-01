be7508aa8a1ec91f00a5458f769d22cd483c9015
Avimitin <avimitin@gmail.com>
2022-11-01 11:04:13 +0800

    Add gC as git commit alias
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: dde9e4ccd398614506ac275cac3b855dee4f9181 [formerly 2a150e8228b550af0f016b84dc3d5c25da2fdc94]
    Former-commit-id: e2ccd643009ed2c6f6a34374ff9aac9a045faad0

--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -16,11 +16,12 @@ end
 
 # G
 if command -q git
-    alias gP "git push"
-    alias gp "git pull"
     alias ga "git add"
     alias gc "git commit -m"
+    alias gC "GIT_EDITOR=$EDITOR git commit -a"
     alias gd "git diff --no-prefix"
+    alias gP "git push"
+    alias gp "git pull"
 end
 
 if command -q lazygit

1e23a63282c82578d09184016ae36eff339226a8
Avimitin <avimitin@gmail.com>
2023-02-15 16:49:18 +0800

    fzf: fix completion preview
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: d0904f8e3b3fe77297b00186065b453b89c0f25c [formerly b085362f5e3d5debe3e69cf30a0680e7a408117a]
    Former-commit-id: 25870d61ab80595602503f89a115beba7cce1de5

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -4,15 +4,15 @@
 export FZF_COMPLETION_TRIGGER=''
 
 if (( $+commands[fd] )); then
-  _fd_argument=('--type f' '--hidden' '--strip-cwd-prefix' '--follow'
+  _fd_argument=('--hidden' '--strip-cwd-prefix' '--follow'
                 '--max-depth 1'
                 '--exclude .git'
                 "--exclude '*.pyc'"
                 '--exclude target/debug'
                 '--exclude target/release')
 
-  export FZF_DEFAULT_COMMAND="fd ${_fd_argument[*]}"
-  export FZF_CTRL_T_COMMAND="fd ${_fd_argument[*]}"
+  export FZF_DEFAULT_COMMAND="fd ${_fd_argument[*]} --type f"
+  export FZF_CTRL_T_COMMAND="fd ${_fd_argument[*]} --type f"
 
   unset _fd_argument
 elif (( $+commands[rg] )); then
@@ -31,11 +31,47 @@ _fzf_opts=(
 export FZF_DEFAULT_OPTS="${_fzf_opts[*]}"
 
 if (( ${+commands[bat]} )); then
-  _fzf_opts=("--preview 'bat -n --color=always {}'"
-    "--bind 'ctrl-/:change-preview-window(down|hidden|)'"
-    ${_fzf_opts[*]})
+  _extra=("--preview 'bat -n --color=always {}'"
+    "--bind 'ctrl-/:change-preview-window(down|hidden|)'")
+  export FZF_CTRL_T_OPTS="${_fzf_opts[*]} ${_extra[*]}"
+  unset _extra
 fi
-export FZF_CTRL_T_OPTS="${_fzf_opts[*]}"
+
+export FZF_COMPLETION_OPTS="${_fzf_opts[*]}"
+
+_fzf_compgen_path() {
+  fd --hidden \
+     --max-depth 2 \
+     --follow \
+     --exclude ".git" \
+     --exclude ".cache" \
+     --exclude target/debug \
+     --exclude target/release \
+     . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --hidden \
+     --follow \
+     --max-depth 2 \
+     --exclude .git \
+     --exclude target/debug \
+     --exclude target/release \
+     --type d \
+     . "$0"
+}
+
+_fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+    "cd"|"j") fzf --preview 'exa -T --color=always --icons -L1 --git' "$@" ;;
+    "export"|"unset") fzf --preview "eval 'echo \$'{}" "$@";;
+    *) fzf --preview 'bat -n --color=always {}' "$@";;
+  esac
+}
 
 if [[ -d "/usr/share/fzf" ]]; then
   local fzf_dir="/usr/share/fzf"

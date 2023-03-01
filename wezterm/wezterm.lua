6471e252025d1602bb9c201461d1f922e63793d2
Avimitin <avimitin@gmail.com>
2023-03-01 13:56:41 +0800

    wezterm: upgrade wezterm config
    Signed-off-by: Avimitin <avimitin@gmail.com>
    
    Former-commit-id: a27b35d0726f6a6811aef36eaebda985715c8756 [formerly ebf5db3d48262f366f8f44248fa338d51468a6fc]
    Former-commit-id: 06ccb2115e6b933cfde89ed654da9619603ca363

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,16 @@
 local api = require("wezterm")
+local action = api.action
 
 local kanagawa = {
 	foreground = "#dcd7ba",
 	background = "#0A0E14",
-
 	cursor_bg = "#c8c093",
 	cursor_fg = "#c8c093",
 	cursor_border = "#c8c093",
-
 	selection_fg = "#c8c093",
 	selection_bg = "#2d4f67",
-
 	scrollbar_thumb = "#16161d",
 	split = "#16161d",
-
 	ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
 	brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
 	indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
@@ -23,9 +20,10 @@ return {
 	font = api.font_with_fallback({
 		"agave Nerd Font Mono",
 		"Noto Sans CJK SC",
-		"Noto Color Emoji"
+		"Noto Color Emoji",
+		"Broot Icons Visual Studio Code",
 	}),
-	font_size = 12,
+	font_size = 10,
 	window_background_opacity = 0.8,
 	text_background_opacity = 0.7,
 	force_reverse_video_cursor = true,
@@ -53,6 +51,26 @@ return {
 				domain = "CurrentPaneDomain",
 			}),
 		},
+		{
+			key = "j",
+			mods = "CTRL|SHIFT",
+			action = action.ActivatePaneDirection("Down"),
+		},
+		{
+			key = "k",
+			mods = "CTRL|SHIFT",
+			action = action.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "l",
+			mods = "CTRL|SHIFT",
+			action = action.ActivatePaneDirection("Right"),
+		},
+		{
+			key = "h",
+			mods = "CTRL|SHIFT",
+			action = action.ActivatePaneDirection("Left"),
+		},
 	},
-	colors = kanagawa
+	colors = kanagawa,
 }

1034febff69a7f7c4643c35c199de03db99e3193
Avimitin <dev@avimit.in>
2023-05-06 11:56:23 +0800

    [fish] Disable launching gpg-agent when ssh
    Signed-off-by: Avimitin <dev@avimit.in>
    
    Former-commit-id: c19d65e5d759b28446c6fadd5f8bc537ca607f61 [formerly 37194f565f5507b4c2f8963a0ba46ba681958601]
    Former-commit-id: 702731297eefe2e8d1523a9d36dda71b11db7442

--- dotfile/fish/conf.d/gpg.fish
+++ dotfile/fish/conf.d/gpg.fish
@@ -1,6 +1,9 @@
 # Use gpg-agent to replace ssh-agent
 if command -q gpgconf
-  set -gx GPG_TTY (tty)
+  if test -z "$SSH_CLIENT" && test -z "$SSH_TTY"
+    set -gx GPG_TTY (tty)
+    gpgconf --launch gpg-agent
+  end
+
   set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-  gpgconf --launch gpg-agent
 end

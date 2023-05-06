b4765f041a25dcd332d3e27191f6de00c43e1a11
Avimitin <dev@avimit.in>
2023-05-06 11:39:43 +0800

    Remove all the fish functions
    Signed-off-by: Avimitin <dev@avimit.in>
    
    Former-commit-id: 088b5f507d16d35f4ae409ba84fb2d66d54942b4 [formerly add36034574ce33f131242f6c6351ba8ed68218c]
    Former-commit-id: b7b22345b3b968daa75ada278e24c77155740411

--- dotfile/fish/functions/fish_config_setup.fish
+++
@@ -1,9 +0,0 @@
-function fish_config_setup
-    curl -sL https://git.io/fisher | source
-
-    fisher install jorgebucaran/fisher
-    fisher install patrickf1/fzf.fish
-    fisher install jorgebucaran/nvm.fish
-    fisher install jorgebucaran/autopair.fish
-    fisher install jorgebucaran/hydro
-end

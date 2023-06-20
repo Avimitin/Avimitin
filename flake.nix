c8ae551758ece656114b109e7bd3f6550ea5902c
Avimitin <dev@avimit.in>
2023-06-20 22:35:39 +0800

    nix: update doc

--- flake.nix
+++ flake.nix
@@ -13,10 +13,12 @@
     in {
       apps = eachSystems ({ pkgs, ... }:
         let
+          # A shell script wrapper to execute nix format for all the nix file in current directory
           my-nix-formatter = (pkgs.writeScriptBin "my-nix-formatter" ''
             find . -name '*.nix' -exec ${pkgs.nixfmt}/bin/nixfmt {} +
           '');
         in {
+          # nix run .#fmt
           fmt = {
             type = "app";
             program = "${my-nix-formatter}/bin/my-nix-formatter";

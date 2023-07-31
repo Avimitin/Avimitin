4cd573800a6865232251eca258cfa51fcb16f07f
Avimitin <dev@avimit.in>
2023-08-01 01:00:23 +0800

    Update flake format
    Signed-off-by: Avimitin <dev@avimit.in>

--- flake.nix
+++ flake.nix
@@ -1,34 +1,23 @@
 {
   description = "Flakes to setup my configuration";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      allSystems = [ "x86_64-linux" ];
-
-      eachSystems = f:
-        nixpkgs.lib.genAttrs allSystems
-        (system: f { pkgs = import nixpkgs { inherit system; }; });
-    in {
-      apps = eachSystems ({ pkgs, ... }:
-        let
-          # A shell script wrapper to execute nix format for all the nix file in current directory
-          my-nix-formatter = (pkgs.writeScriptBin "my-nix-formatter" ''
-            find . -name '*.nix' -exec ${pkgs.nixfmt}/bin/nixfmt {} +
-          '');
-        in {
-          # nix run .#fmt
-          fmt = {
-            type = "app";
-            program = "${my-nix-formatter}/bin/my-nix-formatter";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        formatter = (pkgs.writeScriptBin "my-nix-formatter" ''
+          find . -name '*.nix' -exec ${pkgs.nixfmt}/bin/nixfmt {} +
+        '');
+        templates = {
+          "rust" = {
+            path = ./nix/template/rust;
+            description = "My basic rust project setup";
           };
-        });
-      templates = {
-        "rust" = {
-          path = ./nix/template/rust;
-          description = "My basic rust project setup";
         };
-      };
-    };
+      });
 }

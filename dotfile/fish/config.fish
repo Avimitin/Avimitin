2570e3d6fb1564089fc51c52c9fedd1aca701454
Avimitin <dev@avimit.in>
2023-08-24 18:42:10 +0800

    Update fish config
    Signed-off-by: Avimitin <dev@avimit.in>

--- dotfile/fish/config.fish
+++ dotfile/fish/config.fish
@@ -11,16 +11,17 @@ end
 
 if command -q broot
     function br --wraps=broot
-    set -l cmd_file (mktemp)
-    if broot --outcmd $cmd_file $argv
-        read --local --null cmd < $cmd_file
-        rm -f $cmd_file
-        eval $cmd
-    else
-        set -l code $status
-        rm -f $cmd_file
-        return $code
-    en
+        set -l cmd_file (mktemp)
+        if broot --outcmd $cmd_file $argv
+            read --local --null cmd < $cmd_file
+            rm -f $cmd_file
+            eval $cmd
+        else
+            set -l code $status
+            rm -f $cmd_file
+            return $code
+        end
+    end
 end
 
 if command -q exa

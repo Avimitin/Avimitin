aa9d4c63c6aa42ab1cf48da34be377fe483816b5
Avimitin <dev@avimit.in>
2023-08-25 04:09:54 +0800

    Fix template reference
    Signed-off-by: Avimitin <dev@avimit.in>

--- flake.nix
+++ flake.nix
@@ -7,35 +7,41 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        templates = {
-          "rust" = {
-            path = ./nix/template/rust;
-            description = "My basic rust project setup";
-          };
-          "generic" = {
-            path = ./nix/template/generic;
-            description = "Generic project setup";
-          };
-        };
-        formatter = pkgs.nixpkgs-fmt;
-        apps.init-hm = flake-utils.lib.mkApp {
-          drv = pkgs.writeShellScriptBin "init-home-manager" ''
-            set -e
-            name=$1; shift
-            [[ "$name" = "" ]] \
+    let
+      applyForEachSystem = flake-utils.lib.eachDefaultSystem;
+      systemBaseModule = system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          formatter = pkgs.nixpkgs-fmt;
+          apps.init-hm = flake-utils.lib.mkApp {
+            drv = pkgs.writeShellScriptBin "init-home-manager" ''
+              set -e
+              name=$1; shift
+              [[ "$name" = "" ]] \
               && echo "No file given" && exit 1
-            file="$PWD/nix/home/$name.nix"
-            [[ -f "$file" ]] \
-              || (echo "$file not found" && exit 1)
-            configDir=''${XDG_CONFIG_HOME:-$HOME/.config}
-            mkdir -p $configDir/home-manager
-            ln -s $file $configDir/home-manager/home.nix
-          '';
+                file="$PWD/nix/home/$name.nix"
+                [[ -f "$file" ]] \
+                || (echo "$file not found" && exit 1)
+                  configDir=''${XDG_CONFIG_HOME:-$HOME/.config}
+              mkdir -p $configDir/home-manager
+                ln -s $file $configDir/home-manager/home.nix
+            '';
+          };
         };
-      });
+    in
+    (applyForEachSystem systemBaseModule)
+    // {
+      templates = {
+        "rust" = {
+          path = ./nix/template/rust;
+          description = "My basic rust project setup";
+        };
+        "generic" = {
+          path = ./nix/template/generic;
+          description = "Generic project setup";
+        };
+      };
+    };
 }

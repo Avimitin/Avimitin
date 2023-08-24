58153cda771be8092c9d06dd22c9c0f9d145b189
Avimitin <dev@avimit.in>
2023-08-24 16:09:42 +0800

    Add broot config
    Signed-off-by: Avimitin <dev@avimit.in>

--- dotfile/fish/config.fish
+++ dotfile/fish/config.fish
@@ -9,6 +9,20 @@ if command -q direnv
     direnv hook fish | source
 end
 
+if command -q broot
+    function br --wraps=broot
+    set -l cmd_file (mktemp)
+    if broot --outcmd $cmd_file $argv
+        read --local --null cmd < $cmd_file
+        rm -f $cmd_file
+        eval $cmd
+    else
+        set -l code $status
+        rm -f $cmd_file
+        return $code
+    en
+end
+
 if command -q exa
     alias ll "exa -l -@ -h --icons --group-directories-first"
     alias lt "exa -l -T -L2 --icons"

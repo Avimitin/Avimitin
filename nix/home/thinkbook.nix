097f7f19f37203c22fe7e923d577a849e8e52408
Avimitin <dev@avimit.in>
2023-08-25 13:02:50 +0800

    Build hyprland script in nix
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/home/thinkbook.nix
+++ nix/home/thinkbook.nix
@@ -41,7 +41,37 @@ in
       sha256 = "sha256-xWaMd5POCDeeFTsGtHbIvsPelIp+GZPC1X1CseCo3BA=";
     };
     fontconfig = lib.fromDotfile "fontconfig/conf.d";
-    hyprland = lib.fromDotfile "hypr/hyprland.conf";
+    hyprland =
+      let
+        wpaperd = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "wpaperd";
+          version = "unstable-07-19";
+          src = pkgs.fetchFromGitHub {
+            repo = pname;
+            owner = "danyspin97";
+            rev = "3d45adf8d1b4f3b6c910df371a0183d80f6509d9";
+            sha256 = "sha256-2bAwitXmoz9tC/7BJGqdrA2qq3Bz7j2RkjdwAkMnScc=";
+          };
+          cargoHash = "sha256-NaQmUEMRBMkrmAiY50i0J3ZZNh6PcH2H4fw1UeWaphU=";
+        };
+        screenshot = pkgs.writeShellScriptBin "grim" ''
+          set -e
+          file=$(mktemp "screenshot-XXX-$(date +%F-%T).png")
+          grim -g "$(slurp)" -t png $file
+          wl-copy < $file
+          rm $file
+        '';
+        finalHyprConf = pkgs.runCommand "hyprland-config" { } ''
+          mkdir $out
+          substitute ${../../dotfile/hypr/hyprland.conf} $out/hyprland.conf \
+            --subst-var-by wallpaperDaemon ${wpaperd}/bin/wpaperd \
+            --subst-var-by screenshotScript ${screenshot}/bin/grim
+        '';
+      in
+      {
+        target = "hypr/hyprland.conf";
+        source = "${finalHyprConf}/hyprland.conf";
+      };
     lazygit = lib.fromDotfile "lazygit/config.yml";
     neovim = lib.fromDotfile "nvim";
     nix = lib.fromDotfile "nix/nix.conf";

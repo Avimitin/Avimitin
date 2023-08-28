f28c59b5010c0649e8f581c443daa8a66cf66619
Avimitin <dev@avimit.in>
2023-08-28 14:25:30 +0800

    nix: enhance the home-manager specification
    Signed-off-by: Avimitin <dev@avimit.in>

--- flake.nix
+++ flake.nix
@@ -12,31 +12,32 @@
 
   outputs = { self, nixpkgs, flake-utils, home-manager }:
     let
-      applyHomeConfig = home-manager.lib.homeManagerConfiguration;
+      pkgsIn = system: import nixpkgs { inherit system; };
+      mkHMCfgWith = system: modules:
+        home-manager.lib.homeManagerConfiguration {
+          pkgs = pkgsIn system;
+          inherit modules;
+        };
     in
     {
       templates = import ./nix/templates;
       homeConfigurations = {
-        "homelab" = applyHomeConfig {
-          pkgs = import nixpkgs { system = "x86_64-linux"; };
-          modules = [ ./nix/home/homelab.nix ];
-        };
-        "thinkbook" = applyHomeConfig {
-          pkgs = import nixpkgs { system = "x86_64-linux"; };
-          modules = [ ./nix/home/thinkbook.nix ];
-        };
+        "homelab" = mkHMCfgWith "x86_64-linux" [ ./nix/home/homelab.nix ];
+        "thinkbook" = mkHMCfgWith "x86_64-linux" [ ./nix/home/thinkbook.nix ];
       };
     } //
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = pkgsIn system;
       in
       {
         formatter = pkgs.nixpkgs-fmt;
         apps.hm-switch = flake-utils.lib.mkApp {
-          drv = pkgs.writeShellScriptBin "HomeManagerSwitcher" ''
+          drv = with builtins; pkgs.writeShellScriptBin "HomeManagerSwitcher" ''
             home=$1; shift
-            [[ "x$home" = "x" ]] && echo "No home config given" && exit 1
+            [[ "x$home" = "x" ]] \
+              && echo -e "Error: No home config given.\n\nAvailable: ${concatStringsSep ", " (attrNames self.homeConfigurations)}" \
+              && exit 1
             nix run home-manager/master -- switch --flake ".?submodules=1#$home"
           '';
         };

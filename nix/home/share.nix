5dbc295a7c5c6451136327801ead7256436f759a
Avimitin <dev@avimit.in>
2023-09-11 02:18:07 +0800

    nix: add symlink to load all treesitter plugins
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/home/share.nix
+++ nix/home/share.nix
@@ -45,6 +45,16 @@ in {
       rev = "2ff4940f043cd4ad80fa25c6efa33063fb3b386b";
       sha256 = "sha256-zgFQKQgESThZGoLRjqZGjxeu/C0HMduUOr7jcgELM7s=";
     };
+    # We can't install this file to .config/nvim because it is a symlink to nix store
+    nvim-treesitter-parsers = {
+      # FIXME: Is it better to pin the neovim version instead of refering the local file?
+      source = let
+        parsers = pkgs.callPackage ../../dotfile/nvim/nix/treesitter-parsers.nix {};
+        toNvimPlug = pkgs.callPackage ../../dotfile/nvim/nix/set-rtp.nix {};
+      in
+        toNvimPlug "treesitter-parsers" (parsers []);
+      target = "nvim/site/plugin/treesitter-parsers.lua";
+    };
   };
 
   programs = {

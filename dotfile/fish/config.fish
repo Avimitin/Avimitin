5d312510c007f1b8c738e3c97f213cf4128f1f23
Avimitin <dev@avimit.in>
2023-09-11 12:02:54 +0800

    fish: add nix alias
    Signed-off-by: Avimitin <dev@avimit.in>

--- dotfile/fish/config.fish
+++ dotfile/fish/config.fish
@@ -25,25 +25,9 @@ if command -q broot
 end
 
 if command -q nix
-    function _wrap_nix
-        set -l new_argv
-        for arg in $argv
-            if string match --quiet --regex '^[^\s]+#' $arg
-                set -a new_argv $arg
-            else
-                set -a new_argv ".#$arg"
-            end
-        end
-        echo $new_argv
-    end
-
-    function nixr --wraps "nix run"
-        nix run (_wrap_nix $argv[1]) $argv[2..]
-    end
-
-    function nixd --wraps "nix develop"
-        nix develop (_wrap_nix $argv)
-    end
+    alias nixr "nix run"
+    alias nixl "nix develop"
+    alias nixb "nix build"
 end
 
 if command -q exa

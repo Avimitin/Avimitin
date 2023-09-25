fa5a7f632d5b139299c5415d6421680e8fc982cf
Avimitin <dev@avimit.in>
2023-09-25 10:39:37 +0800

    home-manager: replace thinkbook utilities to nixpkgs ver
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/home/thinkbook.nix
+++ nix/home/thinkbook.nix
@@ -12,6 +12,12 @@ in
     stateVersion = "23.05";
   };
 
+  home.packages = with pkgs; [
+    grim
+    slurp
+    nil
+  ];
+
   xdg.configFile = {
     broot = lib.fromDotfile "broot/conf.toml";
     fcitx5Conf = lib.fromDotfile "fcitx5/conf";
@@ -19,29 +25,18 @@ in
     fontconfig = lib.fromDotfile "fontconfig/conf.d";
     hyprland =
       let
-        wpaperd = pkgs.rustPlatform.buildRustPackage rec {
-          pname = "wpaperd";
-          version = "unstable-07-19";
-          src = pkgs.fetchFromGitHub {
-            repo = pname;
-            owner = "danyspin97";
-            rev = "3d45adf8d1b4f3b6c910df371a0183d80f6509d9";
-            sha256 = "sha256-2bAwitXmoz9tC/7BJGqdrA2qq3Bz7j2RkjdwAkMnScc=";
-          };
-          cargoHash = "sha256-NaQmUEMRBMkrmAiY50i0J3ZZNh6PcH2H4fw1UeWaphU=";
-        };
-        screenshot = pkgs.writeShellScriptBin "grim" ''
+        screenshot = pkgs.writeShellScriptBin "grim-shot" ''
           set -e
-          file=$(mktemp "screenshot-XXX-$(date +%F-%T).png")
-          grim -g "$(slurp)" -t png $file
+          file=$(mktemp --tmpdir "screenshot-XXX-$(date +%F-%T).png")
+          ${pkgs.grim}/bin/grim -g "$(${pkgs.slurp}/bin/slurp)" -t png $file
           wl-copy < $file
           rm $file
         '';
         finalHyprConf = pkgs.runCommand "hyprland-config" { } ''
           mkdir $out
           substitute ${../../dotfile/hypr/hyprland.conf} $out/hyprland.conf \
-            --subst-var-by wallpaperDaemon ${wpaperd}/bin/wpaperd \
-            --subst-var-by screenshotScript ${screenshot}/bin/grim
+            --subst-var-by wallpaperDaemon ${pkgs.wpaperd}/bin/wpaperd \
+            --subst-var-by screenshotScript ${screenshot}/bin/grim-shot
         '';
       in
       {

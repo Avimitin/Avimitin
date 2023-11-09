ce70501c1e83ee673a96a24986c7cf2987f3f492
Avimitin <dev@avimit.in>
2023-11-09 16:56:18 +0800

    fish: update nix alias
    Signed-off-by: Avimitin <dev@avimit.in>

--- dotfile/fish/config.fish
+++ dotfile/fish/config.fish
@@ -27,25 +27,10 @@ if command -q direnv
     direnv hook fish | source
 end
 
-if command -q broot
-    function br --wraps=broot
-        set -l cmd_file (mktemp)
-        if broot --outcmd $cmd_file $argv
-            read --local --null cmd < $cmd_file
-            rm -f $cmd_file
-            eval $cmd
-        else
-            set -l code $status
-            rm -f $cmd_file
-            return $code
-        end
-    end
-end
-
 if command -q nix
-    alias nixr "nix run"
-    alias nixl "nix develop"
-    alias nixb "nix build"
+    alias nr "nix run"
+    alias nl "nix develop"
+    alias nb "nix build"
 end
 
 if command -q lsd

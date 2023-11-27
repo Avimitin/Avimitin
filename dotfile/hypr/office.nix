ef6e3ad5b6836c046de4acc8d718207750ebddc2
Avimitin <dev@avimit.in>
2023-11-27 18:46:07 +0800

    hypr: use 1.5 scale for all monitors
    Signed-off-by: Avimitin <dev@avimit.in>

--- dotfile/hypr/office.nix
+++ dotfile/hypr/office.nix
@@ -4,9 +4,9 @@ writeText "hypr-office.conf" ''
 
   monitor=eDP-1,disable
 
-  # Position is scaled by 2, so to connect two 4K monitor, I need to divide 3840 with 2
-  monitor=DP-5,preferred,0x0,2
-  monitor=HDMI-A-1,preferred,1920x0,2
+  # Position is scaled by 1.5, so to connect two 4K monitor, I need to divide 3840 with 1.5
+  monitor=DP-5,preferred,0x0,1.5
+  monitor=HDMI-A-1,preferred,2560x0,1.5
 
   workspace=1,monitor:DP-5
   workspace=2,monitor:HDMI-A-1

9e9812801bca96ef989bcfd618f3fb3c4f5b3f25
Avimitin <dev@avimit.in>
2024-01-02 14:07:20 +0800

    hyprland: use monitor model name to set up monitors
    Signed-off-by: Avimitin <dev@avimit.in>

--- dotfile/hypr/office.nix
+++ dotfile/hypr/office.nix
@@ -1,13 +1,5 @@
 { writeText }:
 writeText "hypr-office.conf" ''
-  # Hyprland conf for dual monitor
-
   monitor=eDP-1,disable
-
-  # Position is scaled by 1.5, so to connect two 4K monitor, I need to divide 3840 with 1.5
-  monitor=DP-5,preferred,0x0,1.5
-  monitor=HDMI-A-1,preferred,2560x0,1.5
-
-  workspace=1,monitor:DP-5
-  workspace=2,monitor:HDMI-A-1
+  monitor=desc:Stargate Technology SG315 demoset-1,preferred,auto,1.5
 ''

e20aab01c6d48dca796a137b77074c77fc82c97e
Avimitin <dev@avimit.in>
2024-01-31 17:38:14 +0800

    hypr: set scaling to 2 to fix telegram blur text
    Signed-off-by: Avimitin <dev@avimit.in>

--- dotfile/hypr/office.nix
+++ dotfile/hypr/office.nix
@@ -1,5 +1,5 @@
 { writeText }:
 writeText "hypr-office.conf" ''
   monitor=eDP-1,disable
-  monitor=desc:Stargate Technology SG315 demoset-1,preferred,auto,1.5
+  monitor=desc:Stargate Technology SG315 demoset-1,preferred,auto,2
 ''

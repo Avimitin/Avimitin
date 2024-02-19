fa289258ab24b8f7c646e62fea2329205a70c326
Avimitin <dev@avimit.in>
2024-02-19 12:11:25 +0800

    hypr: update office config
    Signed-off-by: Avimitin <dev@avimit.in>

--- dotfile/hypr/office.nix
+++ dotfile/hypr/office.nix
@@ -1,7 +1,7 @@
 { writeText }:
 writeText "hypr-office.conf" ''
   monitor=eDP-1,disable
-  monitor=desc:Stargate Technology SG315 demoset-1,preferred,auto,2
-  env = GDK_SCALE,2
-  env = QT_AUTO_SCREEN_SCALE_FACTOR,2
+  monitor=desc:Stargate Technology SG315 demoset-1,preferred,auto,1
+  env = GDK_SCALE,1
+  env = QT_AUTO_SCREEN_SCALE_FACTOR,1
 ''

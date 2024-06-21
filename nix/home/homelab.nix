5aa845735793aaf954b975d72dee50604ad4bc83
Avimitin <dev@avimit.in>
2024-06-21 12:41:37 +0800

    homelab: update homelab configuration
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/home/homelab.nix
+++ nix/home/homelab.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 let
   lib = import ../lib.nix { inherit pkgs; };
@@ -7,15 +7,52 @@ rec {
   imports = [ ./share.nix ];
 
   home = {
-    username = "i";
-    homeDirectory = "/home/i";
-    stateVersion = "23.05";
+    username = "sh1marin";
+    homeDirectory = "/home/sh1marin";
+    stateVersion = "24.11";
   };
 
-  # This machine also serve as a build cache
-  nix.settings.extra-secret-key-files = "${home.homeDirectory}/.config/nix/nix-cache-sk";
+  home.packages = with pkgs; [
+    grim
+    slurp
+  ];
+
+  xdg.dataFile = {
+    theme = {
+      source = "${../../fcitx/theme}";
+      target = "fcitx5/themes/default";
+    };
+  };
 
   xdg.configFile = {
+    dunst = lib.fromDotfile "dunst/dunstrc";
+    rofi = lib.fromDotfile "rofi";
+    broot = lib.fromDotfile "broot/conf.toml";
+    fontconfig = lib.fromDotfile "fontconfig/conf.d";
+    hyprland = {
+      target = "hypr/hyprland.conf";
+      source =
+        let
+          hypr-conf = pkgs.callPackage ../../dotfile/hypr { };
+        in
+        "${hypr-conf}/hyprland.conf";
+    };
     paru = lib.fromDotfile "paru/paru.conf";
+    waybarConf = lib.fromDotfile "waybar/config";
+    waybarStyle = lib.fromDotfile "waybar/style.css";
+    wezterm = lib.fromDotfile "wezterm/wezterm.lua";
+    kitty = lib.fromDotfile "kitty/kitty.conf";
+    mimeapps = lib.fromDotfile "mimeapps.list";
+    sioyek = lib.fromDotfile "sioyek";
+  };
+
+  programs.wpaperd = {
+    enable = true;
+    settings = {
+      default = {
+        path = "${home.homeDirectory}/Pictures/Wallpapers";
+        duration = "30m";
+      };
+    };
   };
 }

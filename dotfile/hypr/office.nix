886646e5515d68cf746420d23e74953013f3515b
Avimitin <dev@avimit.in>
2024-07-15 01:02:09 +0800

    hypr: try 1.5x scale
    Signed-off-by: Avimitin <dev@avimit.in>

--- dotfile/hypr/office.nix
+++ dotfile/hypr/office.nix
@@ -1,7 +1,6 @@
 { writeText }:
 writeText "hypr-office.conf" ''
-  monitor=eDP-1,disable
-  monitor=desc:Stargate Technology SG315 demoset-1,preferred,auto,1
-  env = GDK_SCALE,1
-  env = QT_AUTO_SCREEN_SCALE_FACTOR,1
+  monitor=DP-1,preferred,auto,1.5
+  env = GDK_SCALE,1.5
+  env = QT_AUTO_SCREEN_SCALE_FACTOR,1.5
 ''

28f45e095242d3b816282147494d219c7c197a75
Avimitin <dev@avimit.in>
2024-07-29 01:38:53 +0800

    hypr: set scale to 2
    Hyprland sadly eat most of my GPU ram when using 1.5x scaling.
    
    Signed-off-by: Avimitin <dev@avimit.in>

--- dotfile/hypr/office.nix
+++ dotfile/hypr/office.nix
@@ -1,6 +1,6 @@
 { writeText }:
 writeText "hypr-office.conf" ''
-  monitor=DP-1,preferred,auto,1.5
-  env = GDK_SCALE,1.5
-  env = QT_AUTO_SCREEN_SCALE_FACTOR,1.5
+  monitor=DP-1,preferred,auto,2
+  env = GDK_SCALE,2
+  env = QT_AUTO_SCREEN_SCALE_FACTOR,2
 ''

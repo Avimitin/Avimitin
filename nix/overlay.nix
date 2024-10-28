090cf4b150ab8febc615547d0173307b11eefbf3
Avimitin <dev@avimit.in>
2024-10-28 20:34:16 +0800

    [deps] bump nixpkgs, nvim, apple fonts
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -7,16 +7,16 @@ final: prev: {
   # sans-serif
   apple-sf-pro = final.mkAppleFonts {
     fontName = "SF-Pro";
-    hash = "sha256-B8xljBAqOoRFXvSOkOKDDWeYUebtMmQLJ8lF05iFnXk=";
+    hash = "sha256-IccB0uWWfPCidHYX6sAusuEZX906dVYo8IaqeX7/O88=";
   };
   # monospace
   apple-sf-mono = final.mkAppleFonts {
     fontName = "SF-Mono";
-    hash = "sha256-Uarx1TKO7g5yVBXAx6Yki065rz/wRuYiHPzzi6cTTl8=";
+    hash = "sha256-bUoLeOOqzQb5E/ZCzq0cfbSvNO1IhW1xcaLgtV2aeUU=";
   };
   # serif
   apple-new-york = final.mkAppleFonts {
     fontName = "NY";
-    hash = "sha256-yYyqkox2x9nQ842laXCqA3UwOpUGyIfUuprX975OsLA=";
+    hash = "sha256-HC7ttFJswPMm+Lfql49aQzdWR2osjFYHJTdgjtuI+PQ=";
   };
 }
